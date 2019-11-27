2fed7c5e46832b566be6832b7fd292611b2e82fd
Simran <s.r.karnani@student.tudelft.nl>
2019-11-27 22:01:44 +0100

    added a BlinkySprite

---
+++ src/main/java/pacman/graphics/sprite/BlinkySprite.java
@@ -0,0 +1,18 @@
+package pacman.graphics.sprite;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import org.jetbrains.annotations.NotNull;
+import pacman.graphics.Style;
+import pacman.logic.entity.Ghost;
+
+public class BlinkySprite extends Sprite<Ghost>{
+
+    @Override
+    public void draw(@NotNull Ghost entity, @NotNull GraphicsContext g, @NotNull Style style,
+                     double t) {
+        Image image= new Image(getClass().getResourceAsStream("images/blinky.png"));
+        g.drawImage(image, 100, 100, image.getWidth()*0.5, image.getHeight()*0.5);
+
+    }
+}
