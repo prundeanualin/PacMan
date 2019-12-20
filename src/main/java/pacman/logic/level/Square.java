a23a61be81dc7423b4844db32d703918ee5bcf85
Ruben Backx <r.w.backx@student.tudelft.nl>
2019-12-20 20:16:25 +0100

    Remove temporary fix

--- src/main/java/pacman/logic/level/Square.java
+++ src/main/java/pacman/logic/level/Square.java
@@ -129,7 +129,7 @@ public class Square {
         }
 
         assert (!solid); // A square with a solid should not be receiving entities.
-        if (entity.isSolid() || entity instanceof Wall) {
+        if (entity.isSolid()) {
             solid = true;
         }
     }
