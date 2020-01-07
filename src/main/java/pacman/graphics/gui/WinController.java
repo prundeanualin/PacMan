dc199845df06fb413f00cb713361b1eaf3586955
Simran <s.r.karnani@student.tudelft.nl>
2020-01-07 12:39:02 -0400

    add win and lose controllers fixed background

---
+++ src/main/java/pacman/graphics/gui/WinController.java
@@ -0,0 +1,21 @@
+package pacman.graphics.gui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WinController implements Initializable {
+
+    @FXML
+    private Text score;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
