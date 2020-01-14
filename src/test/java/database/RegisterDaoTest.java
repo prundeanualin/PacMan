29afd8b7780d441ef23d2090dfd3ebee83b018a0
filipmarchidan <f.t.marchidan@student.tudelft.nl>
2020-01-14 18:08:56 +0100

    change annotations to test

--- src/test/java/database/RegisterDaoTest.java
+++ src/test/java/database/RegisterDaoTest.java
@@ -2,6 +2,7 @@ package database;
 
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
@@ -36,7 +37,7 @@ public class RegisterDaoTest {
 
     private User user;
 
-    @BeforeAll
+    @BeforeEach
     public void setUp() throws SQLException {
         //assertNotNull(dbConnect.getMyConnection());
         userDao = new UserDao();
