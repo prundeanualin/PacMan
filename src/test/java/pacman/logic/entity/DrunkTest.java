0f46f27f948a3cf2995cd2b394bed9822b3b586d
Paul Adriaanse <p.e.a.adriaanse@student.tudelft.nl>
2020-01-23 16:36:07 +0100

    Add Test for key bindings while drunk.

--- src/test/java/pacman/logic/entity/DrunkTest.java
+++ src/test/java/pacman/logic/entity/DrunkTest.java
@@ -1,9 +1,16 @@
 package pacman.logic.entity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import javafx.scene.input.KeyCode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
 import pacman.logic.Direction;
 import pacman.logic.level.Board;
 import pacman.logic.level.MapParser;
@@ -36,7 +43,7 @@ class DrunkTest {
     @Test
     public void becomesDrunk() {
         init("#bP");
-        Assertions.assertFalse(pacMan.isDrunk());
+        assertFalse(pacMan.isDrunk());
 
         pacMan.setNextDirection(Direction.LEFT);
         pacMan.update(0.5);
@@ -54,6 +61,35 @@ class DrunkTest {
         Assertions.assertTrue(pacMan.isDrunk());
 
         pacMan.update(2e-10);
-        Assertions.assertFalse(pacMan.isDrunk());
+        assertFalse(pacMan.isDrunk());
+    }
+
+    /**
+     * Tests keybindings, both original and drunk ones.
+     * This because the keybindings were specifically moved here for drunk behavior.
+     * This in contrast with it being part of the graphics module, which is untested.
+     * @param keyCode the key that would be pressed.
+     * @param direction the direction that pacman would take.
+     */
+    @CsvSource({"A,LEFT", "LEFT,LEFT", "W,UP", "UP,UP", "S,DOWN", "DOWN,DOWN", "D,RIGHT", "RIGHT,RIGHT"})
+    @ParameterizedTest
+    public void testKeyBindingsUnDrunk(KeyCode keyCode, Direction direction) {
+        init("P");
+        assertFalse(pacMan.isDrunk());
+        assertEquals(direction, Direction.keyToDirection(keyCode, pacMan));
+
+        pacMan.setDrunk();
+        assertTrue(pacMan.isDrunk());
+        assertEquals(direction.getInverse(), Direction.keyToDirection(keyCode, pacMan));
+    }
+
+    @EnumSource(KeyCode.class)
+    @ParameterizedTest
+    public void testAllOtherKeys(KeyCode keyCode){
+        init("P");
+        if(!keyCode.isArrowKey() && keyCode!= KeyCode.A && keyCode!= KeyCode.W
+                && keyCode!= KeyCode.S && keyCode!=KeyCode.D){
+            assertNull(Direction.keyToDirection(keyCode, pacMan));
+        }
     }
 }
